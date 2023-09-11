95b78a614541e48595022e38a2ba2cc008e1ed44
OTELLO BOZ MARALDO <oboz@agn.gov.ar>
2023-09-11 08:34:40 -0300

    Shopping Cart

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
+import gamingImg from './assets/gaming-background.jpg';
 
 function App() {
+
+  const gamingBackground = {
+      backgroundImage: `url(${gamingImg})`,
+      backgroundSize: 'cover',
+      height: '100vh',
+      backgroundRepeat: 'no-repeat'
+  };
+
   return (
     <>
     <NavBar />
      <div className="App">
-        <header className="App-header gamingBackground"> 
+        <header className="App-header" style={gamingBackground}> 
           <h1>
             Welcome to GamingFarm.
           </h1>
