9c02af2975ae2032881d4ea6b8adabb576293b40
OTELLO BOZ MARALDO <oboz@agn.gov.ar>
2023-10-20 15:19:41 -0300

    NavDropdown

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,28 +2,31 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import logo from '../../assets/logo.svg';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import CartWidget from '../CartWidget/CartWidget';
-import { Link } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-      <Navbar expand="lg" style={{ background: '#282c34' }}>
-        <Container>
-          <Link to="/">
-            <img src={logo} alt="gamingfarm" width="150" height="150" />
-          </Link>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
-              <Nav.Link href="/" className='text-white'>Inicio</Nav.Link>
-              <Nav.Link href="/games/sports" className='text-white'>Sports</Nav.Link>
-              <Nav.Link href="/games/adventure" className='text-white'>Adventure</Nav.Link>
-              <Nav.Link href="/games/shooters" className='text-white'>Shooters</Nav.Link>
-              <CartWidget />
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+        <Navbar variant="dark" bg="dark" expand="lg">
+          <Container>
+            <Navbar.Brand href="/">
+              <img src={logo} alt="gamingfarm" width="150" height="150" />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbar-dark-example" />
+            <Navbar.Collapse id="navbar-dark-example" className="justify-content-end">
+              <Nav>
+                <NavDropdown id="nav-dropdown-dark-example" title="Categories" menuVariant="dark">
+                  <NavDropdown.Item href="/">Home</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="/games/sports">Sports</NavDropdown.Item>
+                  <NavDropdown.Item href="/games/adventure">Adventure</NavDropdown.Item>
+                  <NavDropdown.Item href="/games/shooters">Shooters</NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+            </Navbar.Collapse>
+            <CartWidget />
+          </Container>
+        </Navbar>
   );
 }
 
